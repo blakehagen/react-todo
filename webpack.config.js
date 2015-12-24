37c195294bfac314ee7d18401b7ec91c17aad256
Blake Hagen <blakehagen@gmail.com>
2015-12-24 14:11:03 -0700

    loaded and working

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  entry: "./app/App.jsx",
-  output: {
-    filename: "public/bundle.jsx"
-  },
-  module: {
-    loaders: [
-      {test: /\.jsx$/, loader: 'jsx-loader'}
-    ]
-  }
+    entry: "./app/app.jsx",
+    output: {
+        filename: "public/bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                include: /app/,
+                exclude: /node_modules/,
+                loader: 'babel?presets[]=react'
+            }
+        ]
+    }
 };
