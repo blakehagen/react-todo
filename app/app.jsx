f95df32e490f3d9292eb006cdf95f5d1e272a3eb
Blake Hagen <blakehagen@gmail.com>
2015-12-24 20:20:44 -0700

    setup

--- app/app.jsx
+++ app/app.jsx
@@ -1,1 +1,17 @@
-console.log('hello world!');
+// console.log('hello world!');
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createClass({
+    render: function(){
+        return (
+            <div>Hello world!</div>
+        );
+    }
+});
+
+
+ReactDOM.render(
+    <App />, document.getElementById('app')
+);
